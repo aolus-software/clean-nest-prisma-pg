91bd5e762b36b3df0c2636ba8acf0a592baf3d98
zulfikar-ditya <zulfikarditya.work@gmail.com>
2025-11-29 20:42:25 +0700

    feat: add prisma and table migrations+seeder

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
+import { successResponse } from "@common/response/response";
 import { Controller, Get, Res } from "@nestjs/common";
+import { DateUtils } from "@utils";
 import { Response } from "express";
 
 @Controller()
 export class AppController {
 	@Get()
 	getHello(@Res() res: Response): Response {
-		return res.json({
-			message: "Welcome to the backend service",
-		});
+		return res.json(
+			successResponse(200, `Welcome to ${process.env.APP_NAME}`, {
+				appName: process.env.APP_NAME,
+				appVersion: process.env.APP_VERSION,
+				timestamp: DateUtils.now().toISOString(),
+			}),
+		);
 	}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { CustomValidationPipe } from "@common/pipes/custom-validation/custom-validation.pipe";
