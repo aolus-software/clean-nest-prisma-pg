b760976e2536afb4ebdada542202f7b039cb7470
Zulfikar Ditya <zulfikarditya.work@gmail.com>
2025-12-23 15:34:29 +0700

    wip: swagger

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,28 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { CustomValidationPipe } from "@common/pipes/custom-validation/custom-validation.pipe";
 import { FastifyAdapter } from "@nestjs/platform-fastify";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { apiReference } from "@scalar/nestjs-api-reference";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule, new FastifyAdapter());
 	app.useGlobalPipes(new CustomValidationPipe());
+
+	const config = new DocumentBuilder()
+		.setTitle(process.env.APP_NAME ?? "clean nest")
+		.setDescription("The api docs for " + process.env.APP_NAME)
+		.setVersion("1.0")
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	app.use(
+		"/reference",
+		apiReference({
+			content: document,
+			withFastify: true,
+			theme: "bluePlanet",
+		}),
+	);
 
 	// CORS Configuration=====================
 	const ALLOWED_ORIGIN = process.env.ALLOWED_ORIGINS?.split(",") ?? [
