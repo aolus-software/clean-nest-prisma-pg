b20dbf5a9ed3fb376d8e5d9b6c8d0747fdd5f3cc
zulfikar-ditya <zulfikarditya.work@gmail.com>
2026-04-18 19:09:48 +0700

    fix(config): wildcard cors origin issue

--- libs/config/src/app/cors.config.ts
+++ libs/config/src/app/cors.config.ts
@@ -1,15 +1,17 @@
 import { getEnv } from "../env";
 
 interface ICorsConfig {
-	origin: string[];
+	origin: string | string[];
 	methods: string[];
 	allowedHeaders: string[];
 	maxAge: number;
 	credentials: boolean;
 }
 
+const isAllowWildCard = getEnv().ALLOWED_ORIGINS.includes("*");
+
 export const CorsConfig: ICorsConfig = {
-	origin: getEnv().ALLOWED_ORIGINS,
+	origin: isAllowWildCard ? "*" : getEnv().ALLOWED_ORIGINS,
 	methods: getEnv().ALLOWED_METHODS,
 	allowedHeaders: getEnv().ALLOWED_HEADERS,
 	maxAge: getEnv().MAX_AGE,
